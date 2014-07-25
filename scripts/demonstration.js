0f8146100b003725f55de7ecb8c75fc19a82208d
Yuriy Rudkovskiy <yur@qapint.com>
2014-07-25 22:08:44 +0300

    Create first structure (WIP)

---
+++ scripts/demonstration.js
@@ -0,0 +1,48 @@
+(function(){
+	"use strict"
+
+	var picGallery = gallery(),
+		picGallery2 = gallery(),
+		content = document.getElementById("content"),
+		structure = "{\
+						\"images\": [\
+							{\
+								\"name\": \"cat\",\
+								\"path\": \"images/cat.png\",\
+								\"description\": \"The best cat ever\",\
+								\"date\": \"2014-07-21T09:05:34.540Z\"\
+							},\
+							{\
+								\"name\": \"dog\",\
+								\"path\": \"images/dog.png\",\
+								\"description\": \"The best dof ever\",\
+								\"date\": \"2014-07-21T09:06:05.544Z\"\
+							},\
+							{\
+								\"name\": \"giraffe\",\
+								\"path\": \"images/giraffe.png\",\
+								\"description\": \"1\",\
+								\"date\": \"2014-07-21T09:07:24.187Z\"\
+							},\
+							{\
+								\"name\": \"dinosaur\",\
+								\"path\": \"images/dinosaur.png\",\
+								\"description\": \"The best dinosaur ever\",\
+								\"date\": \"2014-07-21T09:07:47.683Z\"\
+							}\
+						]\
+					}",
+		structure2 = "{\
+						\"images\": [\
+							{\
+								\"name\": \"dinosaur\",\
+								\"path\": \"images/dinosaur.png\",\
+								\"description\": \"The best dinosaur ever\",\
+								\"date\": \"2014-07-21T09:07:47.683Z\"\
+							}\
+						]\
+					}";
+
+		console.log("This is a demonstration");
+
+})();
---
+++ scripts/gallery.js
@@ -0,0 +1,26 @@
+(function(){
+	"use strict"
+
+	window.gallery = function () {
+
+	    return {
+	    	/*0*/
+	    	storage: null
+
+
+		    /*1*/
+
+		    /*2*/
+
+		    /*3*/
+
+			/*4*/
+
+		    /*5*/
+
+		    /*6*/
+
+	    };
+	};
+
+}());
